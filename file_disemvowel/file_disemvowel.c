c41ce62068f72eb229b80af93b32969b7a476ab4
burde041 <burde041@morris.umn.edu>
2022-10-10 15:13:50 -0500

    Add file_disemvowel.c and remove skip lines in file_disemvowel tests

---
+++ file_disemvowel/file_disemvowel.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <ctype.h>
+#define BUF_SIZE 1024
+
+//A method that checks if a character is a vowel
+bool is_vowel(char c) {
+	char c = tolower(c);
+	if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+		return true;
+	}
+	return false;
+}
+
+//A method that copys all the non-vowels and returns the number of non-vowels
+int copy_non_vowels(int num, char* in_buf, char* out_buf){
+	int num_chars = 0;
+	for(int x = 0; x < num; x++){
+		if(!is_vowel(in_buf[x])){
+			out_buf[num_chars] = in_buf[x];
+			num_chars++;
+		}
+	}
+	return num_chars;
+}
+
+//A method that copys non-vowels from an input to an output
+void disemvowel(FILE* inputFile, FILE* outputFile){
+	char in[BUF_SIZE];
+	char out[BUF_SIZE];
+	while(!feof(input)){
+		int num_chars = fread(in, sizeof(char), BUF_SIZE, inputFile);
+		int non_vowel = copy_non_vowels(num_chars, in, out);
+		fwrite(out, sizeof(char), non_vowel, outputFile);
+	}
+}
+
+//Driver method
+int main(int argc, char *argv[]){
+	FILE *inputFile = stdin;
+	FILE *outputFile = stdout;
+	if(argc == 2) {
+		inputFile = fopen(argv[1], "r");
+	} else if(argc == 3){
+		inputFile = fopen(argv[1], "r");
+		outputFile = fopen(argv[2], "w");
+	} else if(argc > 3){
+		fprintf(stderr);
+		exit(1);
+	}
+	if(inputFile == NULL || outputFile == NULL){
+		fprintf(stderr);
+		exit(1);
+	}
+	disemvowel(inputFile, outputFile);
+	if(inputFile != stdin){
+		fclose(inputFile);
+	}
+	if(outputFile != stdout){
+		fclose(outputFile);
+	}
+	return 0;
+}
