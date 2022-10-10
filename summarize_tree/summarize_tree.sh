343424db8c46353687b74a49fd1b7c0496641c56
burde041 <burde041@morris.umn.edu>
2022-10-10 15:46:13 -0500

    Fix missing parentheses in summarize_tree.sh

--- summarize_tree/summarize_tree.sh
+++ summarize_tree/summarize_tree.sh
@@ -3,6 +3,6 @@
 rootDir="$1"
 
 numDir="$(find "$rootDir" -type d | wc -l | tr -d ' ')"
-numFiles=$(find "$rootDir" -not -type d | wc -l | tr -d ' ')"
+numFiles="$(find "$rootDir" -not -type d | wc -l | tr -d ' ')"
 echo "There were $numDir directories."
 echo "There were $numFiles regular files."
